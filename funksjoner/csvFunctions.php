94f5dcc56fce5a5754ace1c74a2953925368e7d1
Jørgen Andreas Bratland Bull <jorgenabb@gmail.com>
2024-04-25 15:06:43 +0200

    removed pointless comments

--- funksjoner/csvFunctions.php
+++ funksjoner/csvFunctions.php
@@ -16,18 +16,7 @@ class csvFunctions
             $processedCsvArray[$columnKey] = [];
 
             for ($rowKey = 0; $rowKey < count($column); $rowKey++) {
-                //var_dump($j);
-                //echo "<br>";
-                //if ($rowKey === 0) continue;
                 $processedCsvArray[$columnKey][$rawCsvArray[0][$rowKey]] = $column[$rowKey];
-
-/*                if (!isset($processedCsvArray[$i - 1])) {
-                    $processedCsvArray[$i - 1] = [
-                        $j[0] => $j[$i],
-                    ];
-                } else {
-                    $processedCsvArray[$i - 1] += [$j[0] => $j[$i]];
-                }*/
             }
 
         }
@@ -54,29 +43,6 @@ class csvFunctions
             }
         }
         $outputArray = array_merge([$headers], $rowData);
-/*        var_dump($data);
-
-        $transposedArray = array_map(null, ...$rowData);
-
-        // Output array
-        $outputArray = array_map(static function ($row) {
-            return array_values($row);
-        }, $transposedArray);
-
-        foreach ($outputArray as $key => &$i) {
-            array_unshift($i, $headers[$key]);
-        }
-
-        foreach ($outputArray as &$i) {
-            foreach ($i as $j=>$row) {
-                if ($row === null)
-                    unset($i[$j]);
-            }
-
-        }*/
-
-        var_dump($outputArray);
-
         foreach ($outputArray as $key => $fields) {
             fputcsv($file, $fields);
         }
