f78a06373682cbe54bcf07b5d9eb025ad633248a
Jørgen Andreas Bratland Bull <jorgenabb@gmail.com>
2024-05-15 12:51:28 +0200

    Templated bestill.php

--- bestill.php
+++ bestill.php
@@ -1,22 +1,29 @@
 <?php
-include_once "backend/submitFunctions.php";
-if (!isset($_GET["row"])) {
+
+$page["title"] = "Bestilling";
+
+$page["body"] = function () {
+    include_once "backend/submitFunctions.php";
+    if (!isset($_GET["row"])) {
     header("Location: index.php");
     die();
-}
+    }
 
-if (isset($_GET["anntall"])) {
+    if (isset($_GET["anntall"])) {
     submitFunctions::insertIntoTable("bestillinger", [
-        "meny_id" => $_GET["row"],
-        "anntall" => $_GET["anntall"],
+    "meny_id" => $_GET["row"],
+    "anntall" => $_GET["anntall"],
     ]);
     echo "Bestilling sendt inn";
-}
+    }
 
-?>
-<form action="" method="get">
-    <input type="hidden" name="row" value="<?php echo $_GET["row"]; ?>">
-    <label for="anntall">Antall</label>
-    <input type="number" id="anntall" name="anntall" value="1">
-    <button type="submit">Bestill</button>
-</form>
+    ?>
+    <form action="" method="get">
+        <input type="hidden" name="row" value="<?php echo $_GET["row"]; ?>">
+        <label for="anntall">Antall</label>
+        <input type="number" id="anntall" name="anntall" value="1">
+        <button type="submit">Bestill</button>
+    </form>
+<?php };
+
+include('template.php');
